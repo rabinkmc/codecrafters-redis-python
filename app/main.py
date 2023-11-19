6739e52c09aa38773772cbe5f164bb0e0715ae9f
Rabin Dhamala <rabinkmc@gmail.com>
2023-11-20 00:04:27 +0545

    implement get and set method for

--- app/main.py
+++ app/main.py
@@ -1,13 +1,23 @@
 import socket
 from threading import Thread
 
+storage = {}
+
 
 def handle_request(sock):
     while raw_data := sock.recv(1024):
         print(raw_data.decode())
         data = raw_data.decode().strip("\r\n").split("\r\n")
         print(data)
-        if len(data) == 5 and data[2].upper() == "ECHO":
+        if len(data) == 7 and data[2].lower() == "set":
+            key = data[4]
+            value = data[-1]
+            storage[key] = value
+            sock.send(b"+OK\r\n")
+        if len(data) == 5 and data[2].lower() == "get":
+            key = data[-1]
+            sock.send(f"+{storage[key]}\r\n".encode())
+        if len(data) == 5 and data[2].lower() == "echo":
             response = f"+{data[-1]}\r\n"
             sock.send(response.encode())
         else:
