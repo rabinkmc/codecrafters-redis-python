afeb481519bf26e434d1f0c177b09542eeeb96c9
Rabin Dhamala <rabinkmc@gmail.com>
2023-11-19 23:10:57 +0545

    refactor connection logic to separate func

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
 import socket
 
 
+def handle_request(sock):
+    while sock.recv(1024):
+        sock.send(b"+PONG\r\n")
+    sock.close()
+
+
 def main():
     print("Initializing redis server:")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     sock, _ = server_socket.accept()
-    while sock.recv(1024):
-        sock.send(b"+PONG\r\n")
-    sock.close()
+    handle_request(sock)
 
 
 if __name__ == "__main__":
