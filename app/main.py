e060e49101696d98ba55ac17bd9d188a4d183017
Rabin Dhamala <rabinkmc@gmail.com>
2023-11-20 01:03:35 +0545

    remove while

--- app/main.py
+++ app/main.py
@@ -1,27 +1,32 @@
 import socket
 from threading import Thread
 
+
 storage = {}
+BUFFER_SIZE = 1024
 
 
 def handle_request(sock):
-    while raw_data := sock.recv(1024):
-        print(raw_data.decode())
+    while raw_data := sock.recv(BUFFER_SIZE):
         data = raw_data.decode().strip("\r\n").split("\r\n")
-        print(data)
-        if len(data) == 7 and data[2].lower() == "set":
+        print(data, len(data))
+        if b"ping" in raw_data:
+            sock.send(b"+PONG\r\n")
+        elif len(data) == 7 and data[2].lower() == "set":
             key = data[4]
             value = data[-1]
             storage[key] = value
             sock.send(b"+OK\r\n")
-        if len(data) == 5 and data[2].lower() == "get":
+        elif len(data) == 5 and data[2].lower() == "get":
             key = data[-1]
-            sock.send(f"+{storage[key]}\r\n".encode())
-        if len(data) == 5 and data[2].lower() == "echo":
+            value = storage[key]
+            response = f"${len(value)}\r\n{value}\r\n"
+            sock.send(response.encode())
+        elif len(data) == 5 and data[2].lower() == "echo":
             response = f"+{data[-1]}\r\n"
             sock.send(response.encode())
         else:
-            sock.send(b"+PONG\r\n")
+            sock.send(b"-Error: unknown command \r\n")
     sock.close()
 
 
